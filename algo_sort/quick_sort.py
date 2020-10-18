bc568950971516db2d60bba7c5c220eb0e417328
Yoshiya Sugimoto <y.sugimoto@jxpress.net>
2020-10-19 02:19:53 +0900

    Create quick_sort method

--- algo_sort/quick_sort.py
+++ algo_sort/quick_sort.py
@@ -1,1 +1,16 @@
 # クイックソートの実装
+def quick_sort(lis: list):
+    if not lis:
+        return lis
+    p = lis[-1]
+    left = []
+    right = []
+    pivots = []
+    for v in lis:
+        if v < p:
+            left.append(v)
+        elif v == p:
+            pivots.append(v)
+        else:
+            right.append(v)
+    return quick_sort(left) + pivots + quick_sort(right)
